a701986f31f4f20cdbfea79cb2488237b06448c5
emilio <ermsharo@gmail.com>
2022-08-23 17:21:54 -0300

    node server working

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send({
+        "message": "Fullstack Challenge 🏅 - Dictionary"
+    }
+    )
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
